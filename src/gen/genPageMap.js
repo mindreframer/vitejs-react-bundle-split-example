82b889a90ceca7568fd9c43c401243d54dcd00f1
Roman Heinrich <roman.heinrich@gmail.com>
2021-03-31 18:17:42 +0200

    Pagesmap generator

---
+++ src/gen/genPageMap.js
@@ -0,0 +1,21 @@
+const fg = require("fast-glob");
+const fs = require("fs");
+
+let entries = fg.sync(["./src/pages/**/*Page*.tsx"], {
+  dot: true,
+});
+console.log(entries);
+
+entries = entries.map((item) => item.replace("./src/", "./"));
+let lines = [];
+
+lines.push(`export const pagesMap = {`);
+entries.forEach((item) => {
+  let name = item.match(/^\.\/pages\/(.*)\.tsx$/)[1].toLocaleLowerCase();
+  lines.push(`  ${name}: "${item}",`);
+});
+lines.push("};");
+
+// console.log(lines.join("\n"));
+console.log("WRITING TO src/pagesMap.ts");
+fs.writeFileSync("src/pagesMap.ts", lines.join("\n"));
